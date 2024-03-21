8544e587cbcf85e018a2630bb9d8371bc32740e3
hmmini <pak4184@naver.com>
2024-03-21 12:25:55 +0900

    merge projects

---
+++ spring-core02/src/test/java/hello/proxy/pureproxy/concreteproxy/code/TimeProxy.java
@@ -0,0 +1,24 @@
+package hello.proxy.pureproxy.concreteproxy.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TimeProxy extends ConcreteLogic {
+
+    private ConcreteLogic realLogic;
+
+    public TimeProxy(ConcreteLogic realLogic) {
+        this.realLogic = realLogic;
+    }
+
+    @Override
+    public String operation() {
+        log.info("TimeDecorator 실행");
+        long startTime = System.currentTimeMillis();
+        String result = realLogic.operation();
+        long endTime = System.currentTimeMillis();
+        long resultTime = startTime - endTime;
+        log.info("TImeDecorator 종료 resultTime = {}ms", resultTime);
+        return result;
+    }
+}
