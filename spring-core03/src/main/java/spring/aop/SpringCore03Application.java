a3c7b218b0e7aa0faaa8336d43b78343d2d63763
hmmini <pak4184@naver.com>
2024-03-25 01:30:25 +0900

    [Spring Core Advanced]
    - Spring 자동 후처리기

---
+++ spring-core03/src/main/java/spring/aop/SpringCore03Application.java
@@ -0,0 +1,13 @@
+package spring.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCore03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCore03Application.class, args);
+    }
+
+}
