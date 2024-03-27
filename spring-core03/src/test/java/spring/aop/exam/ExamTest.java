dd1bc40f02c8db7ca56a50cd284b6586f7d2be36
hmmini <pak4184@naver.com>
2024-03-27 19:08:12 +0900

    [Spring Core Advanced]
    - @Trace, @Retry 커스텀 어노테이션을 만들기
    - 어노테이션에 AOP를 적용하여 Service, Repository 레이어에 적용하기

---
+++ spring-core03/src/test/java/spring/aop/exam/ExamTest.java
@@ -0,0 +1,26 @@
+package spring.aop.exam;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import spring.aop.exam.aop.RetryAspect;
+import spring.aop.exam.aop.TraceAspect;
+
+@Slf4j
+@Import({TraceAspect.class, RetryAspect.class})
+@SpringBootTest
+public class ExamTest {
+
+    @Autowired
+    ExamService examService;
+
+    @Test
+    void test() {
+        for (int i = 0; i < 5; i++) {
+            log.info("client request i = {}", i);
+            examService.request("data" + i);
+        }
+    }
+}
